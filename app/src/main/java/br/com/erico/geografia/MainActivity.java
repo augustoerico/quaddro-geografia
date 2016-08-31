3dc88b39ceef9c2c9a5d6ab5266a4697a78dad75
augustoerico <augusto.ericosilva@gmail.com>
2016-08-31 20:49:13 -0300

    Finish app

--- app/src/main/java/br/com/erico/geografia/MainActivity.java
+++ app/src/main/java/br/com/erico/geografia/MainActivity.java
@@ -3,10 +3,13 @@ package br.com.erico.geografia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     private RatingBar ratingBar;
     private EditText editText;
     private ListView listView;
+
+    private Brasil[] estados;
+
+    private DeputadosEleitos deputadosEleitos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +32,22 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.editText);
         listView = (ListView) findViewById(R.id.listView);
 
+        estados = Brasil.values();
+        seekBar.setMax(estados.length - 1);
+
+        deputadosEleitos = new DeputadosEleitos();
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Log.i("SeekBar", String.valueOf(progress));
+                Brasil estado = estados[progress];
+
+                editText.setText(estado.getEstado());
+                float rate = Integer.parseInt(estado.getTotal_cidades())/200;
+                ratingBar.setRating(rate);
+
+                loadDeputados(estado.name());
             }
 
             @Override
@@ -42,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void loadDeputados(String estado) {
+        ArrayList<Deputado> deputados = deputadosEleitos.getByState(estado);
+
+        ArrayAdapter<Deputado> adapter = new ArrayAdapter<Deputado>
+                (MainActivity.this, android.R.layout.simple_list_item_1, deputados);
+
+        listView.setAdapter(adapter);
     }
 }
